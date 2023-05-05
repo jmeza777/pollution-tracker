026378b6422c67622a9ec4391a6cf8373013ad85
jmeza777 <super0268@gmail.com>
2023-05-05 15:15:09 -0700

    fixing url .env

--- src/App.js
+++ src/App.js
@@ -31,7 +31,8 @@ const App = () => {
       }
       fetchData();
     }, [dispatch] )
-
+ 
+    
     const [data2, setData2] = useState([])
     useEffect(() => {
       const fetchData2 = async () => {
