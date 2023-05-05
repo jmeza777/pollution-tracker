5da5715cfaf29a944937bf797c1c49e0124f4835
jmeza777 <super0268@gmail.com>
2023-05-05 15:08:04 -0700

    modified store

--- src/index.js
+++ src/index.js
@@ -9,22 +9,22 @@ import {configureStore} from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-//just don't add config and require dotenv.
-
 
 const defaultState = {
   data: {
-  data: []
+    data: []
   }
-}
+};
+
 export const setData = (data) => (
   {
-    type: "SET_DATA", // not an arg, because typos!
+    type: "SET_DATA",
     payload: {
       data
     }
   }  
 );
+
 const dataReducer = (state=defaultState.data, action) => {
   if (!action) return state;
   if (!action.payload) return state;
@@ -38,33 +38,27 @@ const dataReducer = (state=defaultState.data, action) => {
       newState.data = action.payload.data;
       break;
     default:
-      // no change
       break;
   }
+
   return newState;
-}
+};
 
 const rootReducer = combineReducers({
   dataReducer
 });
 
-
-
 const store = configureStore({
-  reducer: rootReducer, 
+  reducer: rootReducer
 });
-window.store = store;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  <Provider store={store}>
-    <App />
+    <Provider store={store}>
+      <App />
     </Provider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
