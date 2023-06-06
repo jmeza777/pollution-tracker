c7de9a3f2759ce4f18c772ce122ded132f033cfa
jmeza777 <super0268@gmail.com>
2023-06-06 14:40:40 -0700

    added copy text to clipboard for coordinates

--- src/components/Citydata.js
+++ src/components/Citydata.js
@@ -1,11 +1,32 @@
 import React from 'react'
+import Button from 'react-bootstrap/Button'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Citydata = ({Citydata}) => {
+
+  const handleCopyClick = async (text) => {
+    try {
+      await navigator.clipboard.writeText(text);
+      console.log('Text copied to clipboard');
+    } catch (err) {
+      console.error('Failed to copy text:', err);
+    }
+  };
   return (
     <>
     <h4 className='city-header'>City: {Citydata.name}</h4>
+    <div className='coord'>
     <p className='city-lat'>Latitude: {Citydata.coord.lat}</p>
+    </div>
+    <div className='lat'>
+    <Button className='lat-but' onClick={() => handleCopyClick(Citydata.coord.lat)}>Copy Latitude</Button>
+    </div>
+    <div className='coord'>
     <p className='city-lon'>Longitude: {Citydata.coord.lon}</p>
+    </div>
+    <div className='lon'>
+    <Button className='lat-but' onClick={() => handleCopyClick(Citydata.coord.lon)}>Copy Longitude</Button>
+    </div>
     <h2>{typeof Citydata != "undefined"}</h2>
     </>
   )
